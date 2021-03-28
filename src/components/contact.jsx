72f10ef76e81aae6126b9a8d026139f8def0de21
Sael Bayona <saelbayona@5ec667ef-2361-4a03-ad60-b0c288e989bb.fritz.box>
2021-03-28 22:19:49 +0200

    Personalizacion de template

--- src/components/contact.jsx
+++ src/components/contact.jsx
@@ -39,10 +39,9 @@ export const Contact = (props) => {
           <div className='col-md-8'>
             <div className='row'>
               <div className='section-title'>
-                <h2>Get In Touch</h2>
+                <h2>Comienza ya!</h2>
                 <p>
-                  Please fill out the form below to send us an email and we will
-                  get back to you as soon as possible.
+                  Hey! te invitamos a que te unas a nuestra causa. Llena el siguiente formulario y dejanos hacer el resto por ti. Entraremos en contacto contigo cuando logremos ofreceter la mejor solucion.
                 </p>
               </div>
               <form name='sentMessage' validate onSubmit={handleSubmit}>
@@ -147,16 +146,7 @@ export const Contact = (props) => {
           </div>
         </div>
       </div>
-      <div id='footer'>
-        <div className='container text-center'>
-          <p>
-            &copy; 2020 Issaaf Kattan React Land Page Template. Design by{' '}
-            <a href='http://www.templatewire.com' rel='nofollow'>
-              TemplateWire
-            </a>
-          </p>
-        </div>
-      </div>
+      
     </div>
   )
 }
